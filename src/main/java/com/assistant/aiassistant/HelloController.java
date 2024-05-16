c329749496cde2b2fbc6ff01394bf54bc57dabcd
Luuk Suijker <105010878+LuukSuijker@users.noreply.github.com>
2024-05-16 14:26:46 +0200

    Add files via upload

---
+++ src/main/java/com/assistant/aiassistant/HelloController.java
@@ -0,0 +1,14 @@
+package com.assistant.aiassistant;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
