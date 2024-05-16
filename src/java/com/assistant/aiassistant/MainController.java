3fc076053bae02312cf9d5cea66da7920698683f
Luuk Suijker <luuk.suijker@gmail.com>
2024-05-16 14:38:24 +0200

    Improve file structure

--- src/java/com/assistant/aiassistant/MainController.java
+++ src/java/com/assistant/aiassistant/MainController.java
@@ -3,7 +3,7 @@ package com.assistant.aiassistant;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
