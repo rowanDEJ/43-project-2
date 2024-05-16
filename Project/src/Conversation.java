670f9819bcddab39436b79b299ccca3a85e75135
rowanDEJ <rowandejong06@gmail.com>
2024-05-16 13:49:51 +0200

    5 classes basis invulling gegeven

--- Project/src/Conversation.java
+++ Project/src/Conversation.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Conversation {
+    private String topic;
+    private ArrayList<String> message = new ArrayList<String>();
+
+    public Conversation(String topic, ArrayList<String> message) {
+        this.topic = topic;
+        this.message = message;
+    }
+
+    public void setTopic(){
+
+    }
+
+    public void message(){
+
+    }
 }
