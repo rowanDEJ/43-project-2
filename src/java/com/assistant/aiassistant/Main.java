d178b433ab04f4578c16dab4e1092d1acf1a3759
Luuk Suijker <luuk.suijker@gmail.com>
2024-05-17 13:33:54 +0200

    Change resolution + start van hoofdscherm

--- src/java/com/assistant/aiassistant/Main.java
+++ src/java/com/assistant/aiassistant/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("43-AI-Assistant");
         stage.setScene(scene);
         stage.show();
