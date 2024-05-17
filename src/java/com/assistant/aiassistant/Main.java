7204d889768e0a8bbfedf411a15adef008f125e5
i-L1ke-Cheese <162706881+i-L1ke-Cheese@users.noreply.github.com>
2024-05-17 15:02:15 +0200

    created query resolution classes, that can be used to make different queryResolutionStrategies, that return a result based on the input query. also an example method that uses these classes to take input and produce output.

--- src/java/com/assistant/aiassistant/Main.java
+++ src/java/com/assistant/aiassistant/Main.java
@@ -1,11 +1,14 @@
 package com.assistant.aiassistant;
 
+import com.assistant.aiassistant.query_API.*;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main extends Application {
     @Override
@@ -17,7 +20,23 @@ public class Main extends Application {
         stage.show();
     }
 
+    public static void executeQuery() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Voer een query in, voor de ai assistent: ");
+        String query = scanner.nextLine();
+
+        QueryResolutionForm<String> queryForm = new QueryResolutionForm<>(query);
+
+        ExamplequeryResolutionStrategy exampleStrategy = new ExamplequeryResolutionStrategy();
+        QueryResolutionResult<String> result = exampleStrategy.resolve(queryForm);
+
+        System.out.println();
+        System.out.println("Query data: " + queryForm.getQueryData());
+        System.out.println("Result data: " + result.getData());
+    }
+
     public static void main(String[] args) {
-        launch();
+        //launch();
+        executeQuery();
     }
 }
