30acdb10e256827b7bd4ac395be217e46b0d07c7
i-L1ke-Cheese <162706881+i-L1ke-Cheese@users.noreply.github.com>
2024-05-17 15:53:51 +0200

    cleanup
    removed example function from main, and put it in a seperate text file.

--- src/java/com/assistant/aiassistant/Main.java
+++ src/java/com/assistant/aiassistant/Main.java
@@ -1,14 +1,11 @@
 package com.assistant.aiassistant;
 
-import com.assistant.aiassistant.query_API.*;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main extends Application {
     @Override
@@ -19,26 +16,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-    public static void executeQuery() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Voer een query in, voor de ai assistent: ");
-        String query = scanner.nextLine();
-
-        QueryResolutionForm<String> queryForm = new QueryResolutionForm<>(query);
-
-        ExamplequeryResolutionStrategy exampleStrategy = new ExamplequeryResolutionStrategy();
-        QueryResolutionResult<String> result = exampleStrategy.resolve(queryForm);
-
-        System.out.println();
-        System.out.println("Query data: " + queryForm.getQueryData());
-        System.out.println("Result data: " + result.getData());
-
-        scanner.close();
-    }
-
     public static void main(String[] args) {
-        //launch();
-        executeQuery();
+        launch();
     }
 }
