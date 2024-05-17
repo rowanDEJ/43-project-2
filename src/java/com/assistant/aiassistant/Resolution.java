ca0b0f32ae383a888285277408dba9db5e16a3b5
rowanDEJ <rowandejong06@gmail.com>
2024-05-17 13:26:56 +0200

    5 classes basis invulling gegeven

---
+++ src/java/com/assistant/aiassistant/Resolution.java
@@ -0,0 +1,11 @@
+package com.assistant.aiassistant;
+
+public class Resolution {
+    private int width;
+    private int height;
+
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
