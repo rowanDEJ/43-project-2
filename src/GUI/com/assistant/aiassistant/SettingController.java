bd7780b15fd39a6dc193a4e08674172ddb2426b9
MeneerMart <102212872+MeneerMart@users.noreply.github.com>
2024-06-05 22:43:49 +0200

    Code werkend gemaakt met het nieuwe settings scherm

--- src/GUI/com/assistant/aiassistant/SettingController.java
+++ src/GUI/com/assistant/aiassistant/SettingController.java
@@ -1,7 +1,6 @@
 package com.assistant.aiassistant;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -12,13 +11,9 @@ public class SettingController {
     @FXML
     public Label showName;
     public TextField password;
-    public CheckBox buttonPassword;
     public TextField email;
-    public CheckBox buttonEmail;
     public TextField firstName;
-    public CheckBox buttonFirstName;
     public TextField lastName;
-    public CheckBox buttonLastName;
 //    public ChoiceBox preferredLanguage;
 //    public CheckBox buttonPreferredLanguage;
 
@@ -34,19 +29,19 @@ public class SettingController {
     public void checkChangable() {
         User activeUser = accountManager.getActiveUser();
 
-        if (buttonFirstName.isSelected() && notNull(firstName.getText())) {
+        if (notNull(firstName.getText())) {
             changePersonalData.changeFirstName(activeUser, firstName.getText());
         }
-        if (buttonLastName.isSelected() && notNull(lastName.getText())) {
+        if (notNull(lastName.getText())) {
             changePersonalData.changeLastName(activeUser, lastName.getText());
         }
-        if (buttonPassword.isSelected() && notNull(password.getText())) {
+        if (notNull(password.getText())) {
             changePersonalData.changePassword(activeUser, password.getText());
         }
-        if (buttonEmail.isSelected() && notNull(email.getText())) {
+        if (notNull(email.getText())) {
             changePersonalData.changeEmail(activeUser, email.getText());
         }
-//        if (buttonPreferredLanguage.isSelected()) {
+//        if (notNull(preferredLanguage.getValue().toString()) {
 //            changePersonalData.changePreferredLanguage(activeUser, preferredLanguage.getValue().toString());
 //        }
     }
